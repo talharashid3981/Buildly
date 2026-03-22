0d3b17bd0f7856a5c1845715602900df68f08629
talharashid3981 <talharashid3981@gmail.com>
2026-03-22 21:42:45 +0500

    Update cookie settings for token in auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 

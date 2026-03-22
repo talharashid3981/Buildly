299b9fb66787ab80be8faa3427f857a2efca20d2
talharashid3981 <talharashid3981@gmail.com>
2026-03-22 21:33:39 +0500

    Update server URL to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Dashboard from './pages/Dashboard'
 import Generate from './pages/Generate'
 import WebsiteEditor from './pages/Editor'
 import Livesite from './pages/Livesite'
-export const serverUrl = "http://localhost:8000"
+export const serverUrl = "https://buildly-ots0.onrender.com"
 
 const App = () => {
   useGetCurrentUser()
